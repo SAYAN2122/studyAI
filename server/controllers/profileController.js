a5c496e32dd2b0a9a37122c6cc82a5de7a7deddb
SAYAN GOEL <sayangoel@SAYANs-MacBook-Air.local>
2026-07-07 09:55:58 +0530

    Fix filename case for Linux deployment

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -1,4 +1,4 @@
-import User from "../models/User.js";
+import User from "../models/user.js";
 import StudySession from "../models/StudySession.js";
 
 export const getProfile = async (req, res) => {
