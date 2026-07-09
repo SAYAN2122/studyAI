41332133c84003c75790bf738225b69139678812
SAYAN GOEL <sayangoel@SAYANs-MacBook-Air.local>
2026-07-09 13:47:25 +0530

    Fix delete study session

--- server/controllers/studySessionController.js
+++ server/controllers/studySessionController.js
@@ -36,3 +36,36 @@ export const getStudySession = async (req, res) => {
 
   }
 };
+// ==========================================
+// Delete Study Session
+// ==========================================
+export const deleteStudySession = async (req, res) => {
+  try {
+    const session = await StudySession.findOneAndDelete({
+      _id: req.params.id,
+      user: req.user._id,
+    });
+
+    if (!session) {
+      return res.status(404).json({
+        success: false,
+        message: "Study Session not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Study Session deleted successfully",
+    });
+
+  } catch (error) {
+
+    console.error(error);
+
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+
+  }
+};
